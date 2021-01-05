bddb1d7058d86b2e5c2de455cc654b073122ccb7
dc morrison <nosirromd@gmail.com>
2021-01-05 22:42:50 +0000

    page67 aka 94 checkpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-reddit';
+  articles: Article[];
+
+  constructor() { 
+    this.articles = [
+      new Article( 'Angular', 'http://angular.io', 3 ),
+      new Article( 'Fullstack', 'http://fullstack.io', 2 ),
+      new Article( 'Angular Homepage', 'https://angular.io', 1 ),
+    ];
+  }
+
+  addArticle (title: HTMLInputElement, link: HTMLInputElement) {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
+  
 }
