25af3e26cd91e71be92fdf92f3e832e350eff1a2
dc morrison <nosirromd@gmail.com>
2021-01-05 23:29:49 +0000

    Re-sorting Based on Score p69 aka 97

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,5 +25,9 @@ export class AppComponent {
     link.value = '';
     return false;
   }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a:Article,b:Article) => b.votes - a.votes);
+  }
   
 }
